a8187a60b97f25bd2a2251fa73989f8d042d2917
Prashant Sawant <prashant.sawant@neosoftmail.com>
2021-08-05 20:38:59 +0530

    tried Enzyme and jest testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+//import { render, screen } from "@testing-library/react";
+// import { shallow } from "enzyme";
+// import App from "./App";
+// import Todo from "./components/Todo";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// describe("rendering components", () => {
+//   it("renders App component without crashing", () => {
+//     shallow(<App />);
+//   });
+//   it("renders Todo component without crashing", () => {
+//     shallow(<Todo />);
+//   });
+// });
--- src/components/__tests__/TodoEnzym.test.js
+++ src/components/__tests__/TodoEnzym.test.js
@@ -1,7 +1,27 @@
-import Enzyme from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import Adapter from "enzyme-adapter-react-16";
+import { configure } from "enzyme";
+import { shallow } from "enzyme";
+import toJson from "enzyme";
+import App from "../../App";
+import Todo from "../Todo";
 
-Enzyme.configure({
-  adapter: new EnzymeAdapter(),
+configure({
+  adapter: new Adapter(),
   disableLifecycleMethods: true,
+});
+
+describe("rendering components", () => {
+  it("renders App component without crashing", () => {
+    shallow(<App />);
+  });
+  it("renders Todo component without crashing", () => {
+    shallow(<Todo />);
+  });
+});
+
+describe("snapshots", () => {
+  it("App snapshot", () => {
+    const tree = shallow(<App />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
 });
